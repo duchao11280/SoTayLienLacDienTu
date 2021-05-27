7065f18718dd7c82a9c81e4a41281b75badcbf14
andt1606 <anga0996@gmail.com>
2021-05-27 14:34:11 +0700

    dialog

--- LopSoTayLienLac/app/src/main/java/com/example/lopsotaylienlac/ui/fee/StudentFeeFragment.java
+++ LopSoTayLienLac/app/src/main/java/com/example/lopsotaylienlac/ui/fee/StudentFeeFragment.java
@@ -1,5 +1,6 @@
 package com.example.lopsotaylienlac.ui.fee;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -8,9 +9,12 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,6 +28,7 @@ import com.example.lopsotaylienlac.beans.Fee;
 import com.example.lopsotaylienlac.beans.Subjectofstudent;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import retrofit2.Call;
@@ -39,6 +44,7 @@ public class StudentFeeFragment extends Fragment {
     private TextView txtPrice;
     SharedPreferences sharedPreferences;
     private int feeofyear;
+    private Button btnStudentFeeOK;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -51,36 +57,59 @@ public class StudentFeeFragment extends Fragment {
         UserApi.apiService.getAllSubclassByStudentID(Integer.parseInt(id)).enqueue(new Callback<ArrayList<Subjectofstudent>>() {
             @Override
             public void onResponse(Call<ArrayList<Subjectofstudent>> call, Response<ArrayList<Subjectofstudent>> responsemain) {
-                UserApi.apiService.getAllFee().enqueue(new Callback<ArrayList<Fee>>() {
-                    @Override
-                    public void onResponse(Call<ArrayList<Fee>> call, Response<ArrayList<Fee>> response) {
-                        List<Fee> lst = new ArrayList<>();
 
-                        lst =  response.body();
-                        int lastindex = lst.size();
-                        Fee lastfee = response.body().get(lastindex-1);
-                        feeofyear = lastfee.getMoney();
+                    UserApi.apiService.getAllFee().enqueue(new Callback<ArrayList<Fee>>() {
+                        @Override
+                        public void onResponse(Call<ArrayList<Fee>> call, Response<ArrayList<Fee>> response) {
+                            List<Fee> lst = new ArrayList<>();
 
-                        studentFeeAdapter= new StudentFeeAdapter(responsemain.body(),feeofyear);
-                        recyclerView.setAdapter(studentFeeAdapter);
-                        recyclerView.setLayoutManager(layoutManager);
-                    }
+                            lst =  response.body();
+                            int lastindex = lst.size();
+                            Fee lastfee = response.body().get(lastindex-1);
+                            feeofyear = lastfee.getMoney();
 
-                    @Override
-                    public void onFailure(Call<ArrayList<Fee>> call, Throwable t) {
+                            studentFeeAdapter= new StudentFeeAdapter(responsemain.body(),feeofyear);
+                            recyclerView.setAdapter(studentFeeAdapter);
+                            recyclerView.setLayoutManager(layoutManager);
+                        }
 
-                    }
-                });
+                        @Override
+                        public void onFailure(Call<ArrayList<Fee>> call, Throwable t) {
+
+                        }
+                    });
 
             }
 
             @Override
             public void onFailure(Call<ArrayList<Subjectofstudent>> call, Throwable t) {
-                Toast.makeText(getContext(),"Không có fee nào",Toast.LENGTH_LONG).show();
+
+                openInfoDialog();
             }
         });
 
         return  root;
+    }
+
+    public void openInfoDialog(){
+        android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(getContext());
+        View view = getLayoutInflater().inflate(R.layout.dialog_student_check_fee,null);
+        AlertDialog alertDialog = builder.create();
+//        builder.setTitle("dsafhuie").setView(view).show();
+        alertDialog.setView(view);
+//        alertDialog.setTitle("Category Form");
+        alertDialog.show();
+
+        /**
+         * su kien nut close
+         */
+        btnStudentFeeOK = view.findViewById(R.id.btnStudentFeeOK);
+        btnStudentFeeOK.setOnClickListener(v -> {
+            alertDialog.cancel();
+//            categoryDao.deleteAll();
+        });
+
+
     }
 
 
