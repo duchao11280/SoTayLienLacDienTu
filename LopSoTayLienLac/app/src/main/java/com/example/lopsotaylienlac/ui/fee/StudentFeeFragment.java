1f5298b103f1a63e522ca0052b653b01c51f3bc6
andt1606 <anga0996@gmail.com>
2021-06-02 08:41:00 +0700

    load fee chưa đóng

--- LopSoTayLienLac/app/src/main/java/com/example/lopsotaylienlac/ui/fee/StudentFeeFragment.java
+++ LopSoTayLienLac/app/src/main/java/com/example/lopsotaylienlac/ui/fee/StudentFeeFragment.java
@@ -73,8 +73,26 @@ public class StudentFeeFragment extends Fragment {
                             int lastindex = lst.size();
                             Fee lastfee = response.body().get(lastindex-1);
                             feeofyear = lastfee.getMoney();
+
+                            //Lấy danh sách học phí chưa đóng
+                            List<Subjectofstudent> templst = new ArrayList<>();
+
+                            for (Subjectofstudent s: responsemain.body()) {
+                                if(s.isPaid()==false){
+                                    templst.add(s);
+                                }
+                            }
+
+                            for (Subjectofstudent cc: templst) {
+                                System.out.println(cc);
+                            }
+                            if(templst.size()==0){
+                                openInfoDialog();
+                                return;
+                            }
+
                             //Set dữ liệu và hiển thị
-                            studentFeeAdapter= new StudentFeeAdapter(responsemain.body(),feeofyear);
+                            studentFeeAdapter= new StudentFeeAdapter(templst,feeofyear);
                             recyclerView.setAdapter(studentFeeAdapter);
                             recyclerView.setLayoutManager(layoutManager);
                         }
